0310bd10916f5a7771dcfdf5cd9676df9e3c94b6
J4ninja <j4nguyen134@gmail.com>
2024-12-09 22:55:10 -0600

    New Changes on 2024-12-09 22:55:10

--- images.py
+++ images.py
@@ -5,7 +5,7 @@ import shutil
 # Paths (using raw strings to handle Windows backslashes correctly)
 posts_dir = r"C:\Users\ngtoa\Documents\ObsidianNotes\content\posts"
 attachments_dir = r"C:\Users\ngtoa\Documents\ObsidianNotes"
-static_images_dir = r"C:\Users\chuck\Documents\chuckblog\static\images"
+static_images_dir = r"C:\Users\ngtoa\Documents\GitHub\johntechtalks\static\images"
 
 # Step 1: Process each markdown file in the posts directory
 for filename in os.listdir(posts_dir):
